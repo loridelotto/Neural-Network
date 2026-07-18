1fdf0b27d03430d79a9892c77d58b1ad9e1c24f6
loridelotto <lorenzo.delotto@mail.polimi.it>
2026-07-18 12:26:08 +0200

    categorical cross entropy loss function + accuracy

--- NN.py
+++ NN.py
@@ -24,6 +24,24 @@ class activation_softmax:
         probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = probabilities
 
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        #number of samples in a batch
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7) #clip data to prevent division by 0
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
 # Create dataset
 X, y = spiral_data(samples=100, classes=3)
 # Create Dense layer with 2 input features and 3 output values
@@ -46,7 +64,20 @@ dense2.forward(activation1.output)
 activation2.forward(dense2.output)
 # Save the output
 output = activation2.output
-print(output[:5])
+
+# Calculate the loss
+loss_function = Loss_CategoricalCrossentropy()
+loss = loss_function.calculate(output, y)
+print('loss: ', loss)
+
+# Check accuracy
+
+predictions = np.argmax(output, axis=1)
+if len(y.shape) == 2:
+    y = np.argmax(y, axis=1)
+# True evaluates to 1; False evaluates to 0
+accuracy = np.mean(predictions == y)
+print('accuracy: ', accuracy*100, '%')
 
 
 
